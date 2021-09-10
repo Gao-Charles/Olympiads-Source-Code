3d377ba32adb7f927c117bb1b6e29d1ebf94ab30
Gao-Charles <charlesgao2019@gmail.com>
2021-09-09 21:01:10 -0400

    commit initial

--- 2021 CCC Junior/2021J2/src/Main2021J2.java
+++ 2021 CCC Junior/2021J2/src/Main2021J2.java
@@ -3,68 +3,22 @@ public class Main2021J2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int bidders = in.nextInt();
-        int temp = 0;
+        int bidders = Integer.parseInt(in.nextLine());
 
-        ArrayList<String> bid = new ArrayList<String>();
-        ArrayList<String> bidPeople = new ArrayList<String>();
-        ArrayList<String> bidAmount = new ArrayList<String>();
+        int winnerAmount = 0;
+        String winnerName = "";
 
-        while (in.hasNextLine()) {
-            bid.add(in.nextLine());
-        }
+        for (int i = 1; i <= bidders; i++) {
+            String tempBidderName = in.nextLine();
+            int tempBidderAmount = Integer.parseInt(in.nextLine());
 
-        bid.remove(0);
-
-        bidPeople.addAll(bid);
-        bidAmount.addAll(bid);
-
-        for (int i = 0; i < bid.size(); i++) {
-            if (i % 2 != 0) {
-                bidPeople.remove(bid.get(i));
-            } else {
-                bidAmount.remove(bid.get(i));
+            if (tempBidderAmount > winnerAmount) {
+                winnerAmount = tempBidderAmount;
+                winnerName = tempBidderName;
             }
         }
 
-        String[] bidAmountString = new String[bidAmount.size()];
-        bidAmountString = bidAmount.toArray(bidAmountString);
-
-        int[] bidAmountInt = Arrays.stream(bidAmountString).mapToInt(Integer::parseInt).toArray();
-
-        for (int x = 0; x < bidders; x++) {
-            for (int y = x + 1; y < bidders; y++) {
-
-                if (bidAmountInt[x] == bidAmountInt[y]) {
-                    temp = bidAmountInt[x];
-                    bidAmountInt[x] = bidAmountInt[y];
-
-                    if (y == bidders - 1) {
-                        String[] bidPeopleString = new String[bidPeople.size()];
-                        bidPeopleString = bidPeople.toArray(bidPeopleString);
-
-                        System.out.println(bidPeopleString[x]);
-
-                        break;
-                    }
-                }
-
-                if (bidAmountInt[x] > bidAmountInt[y]) {
-                    temp = bidAmountInt[x];
-                    bidAmountInt[x] = bidAmountInt[y];
-                    bidAmountInt[y] = temp;
-
-                    if (y == bidders - 1) {
-                        String[] bidPeopleString = new String[bidPeople.size()];
-                        bidPeopleString = bidPeople.toArray(bidPeopleString);
-
-                        System.out.println(bidPeopleString[x]);
-
-                        break;
-                    }
-                }
-            }
-        }
+        System.out.println(winnerName);
     }
 }
 
