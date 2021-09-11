2d127f218296af0d96e09a3b9e72708d18e22c83
Gao-Charles <charlesgao2019@gmail.com>
2021-09-11 13:28:45 -0400

    commit initial

--- 2021 CCC Junior/2021J3/src/Main2021J3.java
+++ 2021 CCC Junior/2021J3/src/Main2021J3.java
@@ -3,47 +3,28 @@ public class Main2021J3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        String tempDirection = "";
-
         while (in.hasNextLine()) {
             String instructions = in.nextLine();
-            String directions = instructions.substring(0, 2);
-            String steps = instructions.substring(instructions.length() - 3, instructions.length());
 
-            String directionsString[] = directions.split(" ");
-            int[] directionsInt = Arrays.stream(directionsString).mapToInt(Integer::parseInt).toArray();
-
-            int count = directionsInt.length;
-
-            int temp = 0;
-            int quotient = 0;
-            int remainder = 0;
             int total = 0;
+            String temp = "";
 
-            for (int i = 0; i < count; i++) {
-                temp = directionsInt[0];
-                quotient = temp / 10;
-                remainder = temp % 10;
-                total = quotient + remainder;
-            }
-
-            if (total == 18) {
-                if (steps == "999") {
-                    break;
-                }
-            } else if (total % 2 == 0) {
-                tempDirection = "right";
-                if (total == 0) {
-                    System.out.println(tempDirection + " " + steps);
-                } else {
-                    System.out.println("right " + steps);
-                }
+            if (instructions.equals("99999")) {
+                break;
             } else {
-                tempDirection = "left";
-                if (total == 0) {
-                    System.out.println(tempDirection + " " + steps);
-                } else {
+                int directions = Integer.parseInt(instructions.substring(0, 2));
+                String steps = instructions.substring(instructions.length() - 3, instructions.length());
+
+                total = (directions / 10) + (directions % 10);
+
+                if (total % 2 == 1) {
                     System.out.println("left " + steps);
+                    temp = "left";
+                } else if (total % 2 == 0) {
+                    System.out.println("right " + steps);
+                    temp = "right";
+                } else {
+                    System.out.println(temp + " " + steps);
                 }
             }
         }
